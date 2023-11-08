be62f6548ea934f6416296770276ed2a3bbd8c69
Alexander Dippe <info@dippe-it.de>
2023-11-08 15:42:24 +0100

    Fix: Remove context instance of validation

--- src/de/jost_net/JVerein/gui/action/SpendenbescheinigungAction.java
+++ src/de/jost_net/JVerein/gui/action/SpendenbescheinigungAction.java
@@ -42,12 +42,6 @@ public class SpendenbescheinigungAction implements Action
   @Override
   public void handleAction(Object context) throws ApplicationException
   {
-    if (context == null || !((context instanceof Spendenbescheinigung)
-        || (context instanceof Mitglied)
-        || (context instanceof MitgliedskontoNode)))
-    {
-      throw new ApplicationException("Kein Element ausgewählt");
-    }
     Spendenbescheinigung spb = null;
 
     try
