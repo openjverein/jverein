98e2f4c48ca96239dbcf6420042da275c2054b20
Johann Maierhofer <johann.maierhofer@freenet.de>
2024-04-10 15:49:43 +0200

    nur wenn Sterbetag aktiv

--- src/de/jost_net/JVerein/util/MitgliedSpaltenauswahl.java
+++ src/de/jost_net/JVerein/util/MitgliedSpaltenauswahl.java
@@ -100,9 +100,19 @@ public class MitgliedSpaltenauswahl extends Spaltenauswahl
     add("Kündigung", "kuendigung", false,
         new DateFormatter(new JVDateFormatTTMMJJJJ()), Column.ALIGN_AUTO,
         false);
-    add("Sterbedatum", "sterbetag", false,
-        new DateFormatter(new JVDateFormatTTMMJJJJ()), Column.ALIGN_AUTO,
-        false);
+    try
+    {
+      if (Einstellungen.getEinstellung().getSterbedatum())
+      {
+        add("Sterbedatum", "sterbetag", false,
+            new DateFormatter(new JVDateFormatTTMMJJJJ()), Column.ALIGN_AUTO,
+            false);
+      }
+    }
+    catch (RemoteException re)
+    {
+      //
+    }
     add("Eingabedatum", "eingabedatum", false,
         new DateFormatter(new JVDateFormatTTMMJJJJ()), Column.ALIGN_AUTO, true);
     add("Letzte Änderung", "letzteaenderung", false,
