8a1ca78e0bc80b6cd4b6fd106a38ecf18d53cb84
Johann Maierhofer <johann.maierhofer@freenet.de>
2024-07-25 17:47:58 +0200

    Update MailControl.java

--- src/de/jost_net/JVerein/gui/control/MailControl.java
+++ src/de/jost_net/JVerein/gui/control/MailControl.java
@@ -21,6 +21,7 @@ import java.io.StringWriter;
 import java.rmi.RemoteException;
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Map;
 import java.util.TreeSet;
@@ -684,8 +685,11 @@ public class MailControl extends FilterControl
     }
     if (isEingabedatumbisAktiv() && getEingabedatumbis().getValue() != null)
     {
-      Date d = (Date) getEingabedatumbis().getValue();
-      mails.addFilter("bearbeitung <= ?", new Object[] { new java.sql.Date(d.getTime()) });
+      Calendar cal = Calendar.getInstance();
+      cal.setTime((Date) getEingabedatumbis().getValue());
+      cal.add(Calendar.DAY_OF_MONTH, 1);
+      mails.addFilter("bearbeitung <= ?", 
+          new Object[] { new java.sql.Date(cal.getTimeInMillis()) });
     }
     if (isDatumvonAktiv() && getDatumvon().getValue() != null)
     {
@@ -694,8 +698,11 @@ public class MailControl extends FilterControl
     }
     if (isDatumbisAktiv() && getDatumbis().getValue() != null)
     {
-      Date d = (Date) getDatumbis().getValue();
-      mails.addFilter("mail.versand <= ?", new Object[] { new java.sql.Date(d.getTime()) });
+      Calendar cal = Calendar.getInstance();
+      cal.setTime((Date) getDatumbis().getValue());
+      cal.add(Calendar.DAY_OF_MONTH, 1);
+      mails.addFilter("mail.versand <= ?", 
+          new Object[] { new java.sql.Date(cal.getTimeInMillis()) });
     }
     mails.setOrder("ORDER BY betreff");
 
