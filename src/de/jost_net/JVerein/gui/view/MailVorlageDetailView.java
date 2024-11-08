c15b6efc5efd20ded7761611c646c5f4f48b14b3
Marlon Müller <marlon130403@gmail.com>
2024-11-08 12:48:15 +0100

    Added Variablen to MailVorlageDetailView.java

--- src/de/jost_net/JVerein/gui/view/MailVorlageDetailView.java
+++ src/de/jost_net/JVerein/gui/view/MailVorlageDetailView.java
@@ -16,16 +16,26 @@
  **********************************************************************/
 package de.jost_net.JVerein.gui.view;
 
+import de.jost_net.JVerein.Variable.MitgliedMap;
 import de.jost_net.JVerein.gui.action.DokumentationAction;
 import de.jost_net.JVerein.gui.control.MailVorlageControl;
+import de.jost_net.JVerein.gui.dialogs.ShowVariablesDialog;
+import de.jost_net.JVerein.gui.menu.ShowVariablesMenu;
+import de.jost_net.JVerein.server.MitgliedImpl;
+import de.jost_net.JVerein.util.LesefeldAuswerter;
+import de.jost_net.JVerein.util.MitgliedDummy;
 import de.willuhn.jameica.gui.AbstractView;
 import de.willuhn.jameica.gui.Action;
 import de.willuhn.jameica.gui.GUI;
 import de.willuhn.jameica.gui.parts.ButtonArea;
 import de.willuhn.jameica.gui.util.LabelGroup;
 import de.willuhn.jameica.gui.util.SimpleContainer;
+import de.willuhn.jameica.system.OperationCanceledException;
+import de.willuhn.logging.Logger;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
 
-public class MailVorlageDetailView extends AbstractView
+public class MailVorlageDetailView extends AbstractView implements Listener
 {
 
   @Override
@@ -43,6 +53,8 @@ public class MailVorlageDetailView extends AbstractView
     ButtonArea buttons = new ButtonArea();
     buttons.addButton("Hilfe", new DokumentationAction(),
         DokumentationUtil.MAILVORLAGE, false, "question-circle.png");
+    buttons.addButton("Variablen anzeigen (F6)",
+            new OpenInsertVariableDialogAction(), null, false, "bookmark.png");
     buttons.addButton("Speichern", new Action()
     {
 
@@ -54,4 +66,49 @@ public class MailVorlageDetailView extends AbstractView
     }, null, true, "document-save.png");
     buttons.paint(this.getParent());
   }
+
+  @Override
+  public void handleEvent(Event event)
+  {
+     if (event.keyCode == org.eclipse.swt.SWT.F6)
+    {
+      new OpenInsertVariableDialogAction().handleAction(null);
+    }
+  }
+
+  private static final class OpenInsertVariableDialogAction implements Action
+  {
+
+    @Override
+    public void handleAction(Object context)
+    {
+      try
+      {
+        LesefeldAuswerter lesefeldAuswerter = new LesefeldAuswerter();
+        lesefeldAuswerter.setLesefelderDefinitionsFromDatabase();
+        lesefeldAuswerter.setMap(new MitgliedMap().getMap(new MitgliedDummy(), null));
+        lesefeldAuswerter.evalAlleLesefelder();
+        ShowVariablesDialog d = new ShowVariablesDialog(
+               lesefeldAuswerter.getMap(), false);
+        ShowVariablesMenu menu = new ShowVariablesMenu();
+        menu.setPrependCopyText("");
+        menu.setAppendCopyText("");
+        d.setContextMenu(menu);
+        d.setDoubleClickAction(menu.getCopyToClipboardAction());
+        d.open();
+      }
+      catch (OperationCanceledException ignored)
+      {
+
+      }
+      catch (Exception e)
+      {
+        Logger.error("Fehler beim Anzeigen der Variablen.", e);
+        GUI.getStatusBar().setErrorText("Fehler beim Anzeigen der Variablen.");
+      }
+
+    }
+
+  }
+
 }
