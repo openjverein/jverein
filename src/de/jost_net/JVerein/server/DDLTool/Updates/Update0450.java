e8f0c5e1dfa2f012edff0de386b75ab5f1bd6c2a
nils <nils@nils-Aspire-E5-511>
2024-11-15 23:24:43 +0100

    Update Umbenannt

--- src/de/jost_net/JVerein/server/DDLTool/Updates/Update0450.java
+++ src/de/jost_net/JVerein/server/DDLTool/Updates/Update0450.java
@@ -20,9 +20,9 @@ import de.willuhn.util.ProgressMonitor;
 
 import java.sql.Connection;
 
-public class Update0449 extends AbstractDDLUpdate
+public class Update0450 extends AbstractDDLUpdate
 {
-  public Update0449(String driver, ProgressMonitor monitor, Connection conn)
+  public Update0450(String driver, ProgressMonitor monitor, Connection conn)
   {
     super(driver, monitor, conn);
   }
