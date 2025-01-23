ba7e39101bf2df7d87deaedb4474956f8362e607
tolot27 <mathias@walter.gd>
2025-01-23 23:50:13 +0100

    Erlaube Kopieren von Inhalt und Ausfüllen von verschlüsselten PDFs Speichere Metadaten unverschlüsselt

--- src/de/jost_net/JVerein/io/FormularAufbereitung.java
+++ src/de/jost_net/JVerein/io/FormularAufbereitung.java
@@ -154,8 +154,9 @@ public class FormularAufbereitung
         if (encrypt)
         {
           writer.setEncryption(null, null,
-              PdfWriter.ALLOW_PRINTING | PdfWriter.ALLOW_SCREENREADERS,
-              PdfWriter.ENCRYPTION_AES_256);
+              PdfWriter.ALLOW_PRINTING | PdfWriter.ALLOW_SCREENREADERS
+                  | PdfWriter.ALLOW_COPY | PdfWriter.ALLOW_FILL_IN,
+              PdfWriter.ENCRYPTION_AES_256 | PdfWriter.DO_NOT_ENCRYPT_METADATA);
           doc.open();
         }
       }
