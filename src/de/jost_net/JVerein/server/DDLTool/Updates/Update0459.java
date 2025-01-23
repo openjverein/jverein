c4fc68184091687c247cb9bc1a1849048e23c6a7
nils <nils.henken@posteo.de>
2025-01-23 17:35:28 +0100

    Update umbenannt

--- src/de/jost_net/JVerein/server/DDLTool/Updates/Update0459.java
+++ src/de/jost_net/JVerein/server/DDLTool/Updates/Update0459.java
@@ -20,9 +20,9 @@ import de.jost_net.JVerein.server.DDLTool.Column;
 import de.willuhn.util.ApplicationException;
 import de.willuhn.util.ProgressMonitor;
 
-public class Update0458 extends AbstractDDLUpdate
+public class Update0459 extends AbstractDDLUpdate
 {
-  public Update0458(String driver, ProgressMonitor monitor, Connection conn)
+  public Update0459(String driver, ProgressMonitor monitor, Connection conn)
   {
     super(driver, monitor, conn);
   }
