2f50b656544ffb8c5de4bdad198e49edf0b209af
Johann Maierhofer <johann.maierhofer@freenet.de>
2025-02-01 15:35:34 +0100

    Zweite Sortierung im Report nach spendedatum

--- src/de/jost_net/JVerein/gui/control/SpendenbescheinigungControl.java
+++ src/de/jost_net/JVerein/gui/control/SpendenbescheinigungControl.java
@@ -626,7 +626,7 @@ public class SpendenbescheinigungControl extends DruckMailControl
     DBIterator<Spendenbescheinigung> list = Einstellungen.getDBService()
         .createList(Spendenbescheinigung.class);
     list.addFilter("id in (" + StringUtils.join(ids, ",") + ")");
-    list.setOrder(" ORDER BY bescheinigungsdatum desc ");
+    list.setOrder(" ORDER BY bescheinigungsdatum desc, spendedatum desc ");
     ArrayList<Spendenbescheinigung> spendenbescheinigungen = list != null
         ? (ArrayList<Spendenbescheinigung>) PseudoIterator.asList(list)
         : null;
