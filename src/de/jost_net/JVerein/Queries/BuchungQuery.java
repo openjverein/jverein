1823f3cfa1a437f6e6f046b2a1e511ea026c62a2
nils <nils.henken@posteo.de>
2025-02-02 23:39:02 +0100

    geprueft auf null testen

--- src/de/jost_net/JVerein/Queries/BuchungQuery.java
+++ src/de/jost_net/JVerein/Queries/BuchungQuery.java
@@ -247,7 +247,7 @@ public class BuchungQuery
 
     if (ungeprueft)
     {
-      it.addFilter("geprueft = 0");
+      it.addFilter("geprueft = 0 or geprueft is null");
     }
 
     if (betrag != null && betrag.length() > 0)
