bb1b1dc2b3c85ca1cd30d3fd414c7b8fa80a6427
nils <nils.henken@posteo.de>
2025-02-08 12:12:46 +0100

    Update umbenannt

--- src/de/jost_net/JVerein/server/DDLTool/Updates/Update0464.java
+++ src/de/jost_net/JVerein/server/DDLTool/Updates/Update0464.java
@@ -20,9 +20,9 @@ import de.willuhn.util.ProgressMonitor;
 
 import java.sql.Connection;
 
-public class Update0463 extends AbstractDDLUpdate
+public class Update0464 extends AbstractDDLUpdate
 {
-  public Update0463(String driver, ProgressMonitor monitor, Connection conn)
+  public Update0464(String driver, ProgressMonitor monitor, Connection conn)
   {
     super(driver, monitor, conn);
   }
