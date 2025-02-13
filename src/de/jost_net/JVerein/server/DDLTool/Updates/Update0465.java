69b28eb5099e591b0370958e120ff24aceabfd74
Marlon Müller <marlon130403@gmail.com>
2025-02-13 12:26:23 +0100

    Incremented Update0465.java

--- src/de/jost_net/JVerein/server/DDLTool/Updates/Update0465.java
+++ src/de/jost_net/JVerein/server/DDLTool/Updates/Update0465.java
@@ -23,9 +23,9 @@ import de.willuhn.util.ProgressMonitor;
 import java.sql.Connection;
 
 @SuppressWarnings("unused")
-public class Update0462 extends AbstractDDLUpdate
+public class Update0465 extends AbstractDDLUpdate
 {
-  public Update0462(String driver, ProgressMonitor monitor, Connection conn)
+  public Update0465(String driver, ProgressMonitor monitor, Connection conn)
   {
     super(driver, monitor, conn);
   }
