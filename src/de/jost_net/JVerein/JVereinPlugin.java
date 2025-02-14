8b2c79130908e65b0953999aa58919a3c3fd91f3
tolot27 <mathias@walter.gd>
2025-02-14 16:16:33 +0100

    demonstrate base workflow action

--- src/de/jost_net/JVerein/JVereinPlugin.java
+++ src/de/jost_net/JVerein/JVereinPlugin.java
@@ -9,7 +9,7 @@
  *  the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along with this program.  If not, 
- * see <http://www.gnu.org/licenses/>.
+ * see <http://www.gnu.org/licenses>.
  * 
  * heiner@jverein.de
  * www.jverein.de
