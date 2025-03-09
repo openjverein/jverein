202d1a52f44ce22e190b2116b04c71188e2248b2
nils <nils.henken@posteo.de>
2025-03-09 19:47:27 +0100

    View Umbenannt

--- src/de/jost_net/JVerein/gui/view/BuchungsartDetailView.java
+++ src/de/jost_net/JVerein/gui/view/BuchungsartDetailView.java
@@ -24,7 +24,7 @@ import de.willuhn.jameica.gui.GUI;
 import de.willuhn.jameica.gui.parts.ButtonArea;
 import de.willuhn.jameica.gui.util.LabelGroup;
 
-public class BuchungsartView extends AbstractView
+public class BuchungsartDetailView extends AbstractView
 {
 
   @Override
