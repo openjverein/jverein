63c55c3e6abf93d324b0805432d16d25bb332e12
nils <nils.henken@posteo.de>
2025-03-09 12:40:20 +0100

    Auch bei Splitbuchungen keine History

--- src/de/jost_net/JVerein/gui/action/SplitbuchungNeuAction.java
+++ src/de/jost_net/JVerein/gui/action/SplitbuchungNeuAction.java
@@ -53,6 +53,11 @@ public class SplitbuchungNeuAction implements Action
       buch.setSpeicherung(false);
       buch.setSplitTyp(SplitbuchungTyp.SPLIT);
       buch.setBetrag(SplitbuchungsContainer.getSumme(SplitbuchungTyp.HAUPT).doubleValue() - SplitbuchungsContainer.getSumme(SplitbuchungTyp.SPLIT).doubleValue());
+
+      // Wenn CurrentObject und View von aktueller und nächster View gleich
+      // sind, wird die aktuelle View nicht in die History aufgenommen. Dadurch
+      // führt der Zurückbutton auch bei "Speichern und neu" zur Liste zurück.
+      GUI.getCurrentView().setCurrentObject(buch);
       GUI.startView(BuchungView.class, buch);
     }
     catch (RemoteException e)
