894b44718ff5e977d5c2d108668dd36384840daf
Johann Maierhofer <johann.maierhofer@freenet.de>
2025-04-01 17:31:46 +0200

    Buchungen ohne Buchungsart im Saldo anzeigen

--- src/de/jost_net/JVerein/gui/parts/AbstractSaldoList.java
+++ src/de/jost_net/JVerein/gui/parts/AbstractSaldoList.java
@@ -95,6 +95,7 @@ public class AbstractSaldoList extends TablePart
       return Double.valueOf(rs.getDouble(1));
     }
   };
+
   ResultSetExtractor rsi = new ResultSetExtractor()
   {
     @Override
@@ -209,19 +210,35 @@ public class AbstractSaldoList extends TablePart
         new BuchungsklasseSaldoZeile(BuchungsklasseSaldoZeile.GESAMTSALDOFOOTER,
             "Saldo aller Buchungsklassen ", suEinnahmen, suAusgaben,
             suUmbuchungen));
+
+    // Buchungen ohne Buchungsart
+    String sqlOhneBuchungsart = "SELECT sum(buchung.betrag) FROM buchung, konto "
+        + "WHERE datum >= ? AND datum <= ? AND buchung.konto = konto.id "
+        + "AND konto.kontoart < ? AND buchung.buchungsart is null";
+    Double ohneBuchungsart = (Double) service.execute(sqlOhneBuchungsart,
+        new Object[] { datumvon, datumbis, Kontoart.LIMIT.getKey() }, rsd);
+
+    if (ohneBuchungsart >= 0.01d || ohneBuchungsart <= -0.01d)
+    {
+      zeile.add(new BuchungsklasseSaldoZeile(
+          BuchungsklasseSaldoZeile.GESAMTGEWINNVERLUST,
+          "Saldo Buchungen ohne Buchungsart ", ohneBuchungsart));
+    }
+
     zeile.add(new BuchungsklasseSaldoZeile(
         BuchungsklasseSaldoZeile.GESAMTGEWINNVERLUST, "Gesamtsaldo ",
-        suEinnahmen + suAusgaben + suUmbuchungen));
+        suEinnahmen + suAusgaben + suUmbuchungen + ohneBuchungsart));
 
     // Gesamtübersicht Steuern ausgeben
     getSteuerUebersicht(zeile);
 
-    // Buchungen ohne Buchungsart
-    String sql = "select count(*) from buchung " + "where datum >= ? and datum <= ?  "
-        + "and buchung.buchungsart is null";
+    // Anzahl Buchungen ohne Buchungsart
+    String sql = "SELECT count(*) FROM buchung, konto "
+        + "WHERE datum >= ? AND datum <= ? AND buchung.konto = konto.id "
+        + "AND konto.kontoart < ? AND buchung.buchungsart is null";
 
     Integer anzahl = (Integer) service.execute(sql,
-        new Object[] { datumvon, datumbis }, rsi);
+        new Object[] { datumvon, datumbis, Kontoart.LIMIT.getKey() }, rsi);
     if (anzahl > 0)
     {
       zeile.add(new BuchungsklasseSaldoZeile(
