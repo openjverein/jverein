b9562fa569c46830797ab72175442e771bbbbacb
Johann Maierhofer <johann.maierhofer@freenet.de>
2025-09-02 14:47:23 +0200

    Kommentare

--- src/de/jost_net/JVerein/server/ZusatzbetragImpl.java
+++ src/de/jost_net/JVerein/server/ZusatzbetragImpl.java
@@ -350,18 +350,6 @@ public class ZusatzbetragImpl extends AbstractJVereinDBObject
   @Override
   public Object getAttribute(String fieldName) throws RemoteException
   {
-    if ("id-int".equals(fieldName))
-    {
-      try
-      {
-        return Integer.valueOf(getID());
-      }
-      catch (Exception e)
-      {
-        Logger.error("unable to parse id: " + getID());
-        return getID();
-      }
-    }
     if (fieldName.equals("intervalltext"))
     {
       return getIntervallText();
