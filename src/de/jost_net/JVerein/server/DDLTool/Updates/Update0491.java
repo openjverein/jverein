baf1222d7b3b53addf3f728e6ccbb491394901d1
Johann Maierhofer <johann.maierhofer@freenet.de>
2025-10-07 15:42:26 +0200

    Update Update0491.java

--- src/de/jost_net/JVerein/server/DDLTool/Updates/Update0491.java
+++ src/de/jost_net/JVerein/server/DDLTool/Updates/Update0491.java
@@ -20,9 +20,9 @@ import de.jost_net.JVerein.server.DDLTool.Column;
 import de.willuhn.util.ApplicationException;
 import de.willuhn.util.ProgressMonitor;
 
-public class Update0489 extends AbstractDDLUpdate
+public class Update0491 extends AbstractDDLUpdate
 {
-  public Update0489(String driver, ProgressMonitor monitor, Connection conn)
+  public Update0491(String driver, ProgressMonitor monitor, Connection conn)
   {
     super(driver, monitor, conn);
   }
