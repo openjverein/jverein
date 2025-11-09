8a4fcaf8bdc5726181d948cc021c4f84de141df7
lenilsas <167138849+lenilsas@users.noreply.github.com>
2025-11-09 17:53:53 +0100

    Formularfelder mit Velocity (#1069)
    * Formularfelder mit Velocity
    
    * Import und Export angepasst
    
    * Ausrichtung fehler behoben

--- src/de/jost_net/JVerein/Variable/SpendenbescheinigungMap.java
+++ src/de/jost_net/JVerein/Variable/SpendenbescheinigungMap.java
@@ -471,7 +471,7 @@ public class SpendenbescheinigungMap extends AbstractMap
     map.put(SpendenbescheinigungVar.VERANLAGUNGSZEITRAUM.getName(),
         "2022 bis 2024");
     map.put(SpendenbescheinigungVar.ZWECK.getName(), "Spende");
-    map.put(SpendenbescheinigungVar.UNTERSCHRIFT.getName(), "Unterschrift");
+
     map.put(SpendenbescheinigungVar.ZEILE1.getName(), "Herr");
     map.put(SpendenbescheinigungVar.ZEILE2.getName(), "Willi Wichtig");
     map.put(SpendenbescheinigungVar.ZEILE3.getName(), "Bahnhofstr. 22");
