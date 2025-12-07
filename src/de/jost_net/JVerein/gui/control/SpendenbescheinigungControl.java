fc15fc2c88e083b74acc2466e5dc103643fc6423
Johann Maierhofer <johann.maierhofer@freenet.de>
2025-12-07 15:54:03 +0100

    Feld Bezeichnung Sachspende vergrößert

--- src/de/jost_net/JVerein/gui/control/SpendenbescheinigungControl.java
+++ src/de/jost_net/JVerein/gui/control/SpendenbescheinigungControl.java
@@ -87,6 +87,7 @@ import de.willuhn.jameica.gui.input.DateInput;
 import de.willuhn.jameica.gui.input.DecimalInput;
 import de.willuhn.jameica.gui.input.Input;
 import de.willuhn.jameica.gui.input.SelectInput;
+import de.willuhn.jameica.gui.input.TextAreaInput;
 import de.willuhn.jameica.gui.input.TextInput;
 import de.willuhn.jameica.gui.parts.Button;
 import de.willuhn.jameica.gui.parts.table.FeatureSummary;
@@ -131,7 +132,7 @@ public class SpendenbescheinigungControl extends DruckMailControl
 
   private CheckboxInput ersatzaufwendungen;
 
-  private TextInput bezeichnungsachzuwendung;
+  private TextAreaInput bezeichnungsachzuwendung;
 
   private SelectInput herkunftspende;
 
@@ -388,14 +389,15 @@ public class SpendenbescheinigungControl extends DruckMailControl
     return ersatzaufwendungen;
   }
 
-  public TextInput getBezeichnungSachzuwendung() throws RemoteException
+  public TextAreaInput getBezeichnungSachzuwendung() throws RemoteException
   {
     if (bezeichnungsachzuwendung != null)
     {
       return bezeichnungsachzuwendung;
     }
-    bezeichnungsachzuwendung = new TextInput(
-        getSpendenbescheinigung().getBezeichnungSachzuwendung(), 100);
+    bezeichnungsachzuwendung = new TextAreaInput(
+        getSpendenbescheinigung().getBezeichnungSachzuwendung(), 1000);
+    bezeichnungsachzuwendung.setHeight(50);
     bezeichnungsachzuwendung.disable();
     return bezeichnungsachzuwendung;
   }
