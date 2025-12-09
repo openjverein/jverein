73fb13943d5c2e5b6905b940cd4148346367734c
Johann Maierhofer <126261667+JohannMaierhofer@users.noreply.github.com>
2025-12-09 10:16:49 +0100

    Feld Bezeichnung Sachspende vergrößert (#1218)
    Co-authored-by: Johann Maierhofer <johann.maierhofer@freenet.de>

--- src/de/jost_net/JVerein/server/SpendenbescheinigungImpl.java
+++ src/de/jost_net/JVerein/server/SpendenbescheinigungImpl.java
@@ -380,7 +380,7 @@ public class SpendenbescheinigungImpl extends AbstractJVereinDBObject
 
   /**
    * Liefert als Kennzeichen zurück, ob die Spendenbescheinigung eine echte
-   * Geldspende ist. Dies ist der Fall, wenn es sich um eine Gelspende handelt
+   * Geldspende ist. Dies ist der Fall, wenn es sich um eine Geldspende handelt
    * bei der bei keiner Buchung das Flag Erstattungsverzicht gesetzt ist.
    * 
    * @return Flag, ob echte Geldspende
