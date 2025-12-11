46c5d92efa059fa97a2f4dc9815ae836c5a478dc
Johann Maierhofer <johann.maierhofer@freenet.de>
2025-12-11 21:39:19 +0100

    Update Update0499.java

--- src/de/jost_net/JVerein/server/DDLTool/Updates/Update0499.java
+++ src/de/jost_net/JVerein/server/DDLTool/Updates/Update0499.java
@@ -20,9 +20,9 @@ import de.willuhn.util.ProgressMonitor;
 
 import java.sql.Connection;
 
-public class Update0498 extends AbstractDDLUpdate
+public class Update0499 extends AbstractDDLUpdate
 {
-  public Update0498(String driver, ProgressMonitor monitor, Connection conn)
+  public Update0499(String driver, ProgressMonitor monitor, Connection conn)
   {
     super(driver, monitor, conn);
   }
