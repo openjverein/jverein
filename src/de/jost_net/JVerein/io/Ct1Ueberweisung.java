19760b8c23b9db82faba40cc6107e78e8191c06e
lenilsas <167138849+lenilsas@users.noreply.github.com>
2025-12-21 11:12:44 +0100

    1CT Überweisung Kontoinhaber verwenden (#1230)
    * 1CT Überweisung Kontoinhaber verwenden
    
    * Doppelt entfernt
    
    * String Tool

--- src/de/jost_net/JVerein/io/Ct1Ueberweisung.java
+++ src/de/jost_net/JVerein/io/Ct1Ueberweisung.java
@@ -202,8 +202,21 @@ public class Ct1Ueberweisung
         ad.setIban(ls.getIBAN());
         ue.setGegenkonto(ad);
         ue.setEndtoEndId(ls.getMandatID());
-        ue.setGegenkontoName(StringTool
-            .getStringWithMaxLength(Zeichen.convert(ls.getName()), 255));
+        if (ls.getMitglied() != null)
+        {
+          ue.setGegenkontoName(
+              StringTool.getStringWithMaxLength(
+                  Zeichen.convert(ls.getMitglied()
+                      .getKontoinhaber(Mitglied.namenformat.KONTOINHABER)),
+                  255));
+        }
+        else if (ls.getKursteilnehmer() != null)
+        {
+          ue.setGegenkontoName(StringTool.getStringWithMaxLength(
+              Zeichen.convert(
+                  Adressaufbereitung.getNameVorname(ls.getKursteilnehmer())),
+              255));
+        }
         ue.setTermin(faell);
         ue.setZweck(StringTool.getStringWithMaxLength(
             Zeichen.convert(eval(ls, verwendungszweck)), 140));
