51f0b0180a74d4cf5af504b31c4c79e3789754ca
lenilsas <167138849+lenilsas@users.noreply.github.com>
2026-03-11 13:20:57 +0100

    Mitgleidstyp schon in MitgliedDetailAction setzen

--- src/de/jost_net/JVerein/gui/action/MitgliedDetailAction.java
+++ src/de/jost_net/JVerein/gui/action/MitgliedDetailAction.java
@@ -133,6 +133,7 @@ public class MitgliedDetailAction implements Action
         {
           mitglied.setPersonenart("n");
         }
+        mitglied.setMitgliedstyp(Long.valueOf(Mitgliedstyp.MITGLIED));
       }
       // Wenn CurrentObject und View von aktueller und nächster View gleich
       // sind, wird die aktuelle View nicht in die History aufgenommen. Dadurch
