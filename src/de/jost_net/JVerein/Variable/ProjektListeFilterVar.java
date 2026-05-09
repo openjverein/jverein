e58d3a85a6d7d063a0d46e5524a89e39caefbdfd
Johann Maierhofer <johann.maierhofer@freenet.de>
2026-05-09 10:23:44 +0200

    Kommentar

--- src/de/jost_net/JVerein/Variable/ProjektListeFilterVar.java
+++ src/de/jost_net/JVerein/Variable/ProjektListeFilterVar.java
@@ -1,5 +1,4 @@
 /**********************************************************************
- * Copyright (c) by Heiner Jostkleigrewe
  * This program is free software: you can redistribute it and/or modify it under the terms of the
  * GNU General Public License as published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
@@ -11,8 +10,6 @@
  * You should have received a copy of the GNU General Public License along with this program.  If not,
  * see <http://www.gnu.org/licenses/>.
  *
- * heiner@jverein.de
- * www.jverein.de
  **********************************************************************/
 package de.jost_net.JVerein.Variable;
 
