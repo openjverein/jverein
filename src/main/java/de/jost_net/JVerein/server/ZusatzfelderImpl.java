eb186033600e9db35e63b4f70a6a37dfc9fa08b7
lenilsas <167138849+lenilsas@users.noreply.github.com>
2026-07-30 15:49:52 +0200

    Personalbogen nicht null ausgeben

--- src/main/java/de/jost_net/JVerein/server/ZusatzfelderImpl.java
+++ src/main/java/de/jost_net/JVerein/server/ZusatzfelderImpl.java
@@ -214,8 +214,16 @@ public class ZusatzfelderImpl extends AbstractDBObject implements Zusatzfelder
             return "";
           }
         case Datentyp.GANZZAHL:
-          return getFeldGanzzahl() + "";
+          if (getFeldGanzzahl() == null)
+          {
+            return "";
+          }
+          return getFeldGanzzahl().toString();
         case Datentyp.JANEIN:
+          if (getFeldJaNein() == null)
+          {
+            return "";
+          }
           return getFeldJaNein() ? "ja" : "nein";
         case Datentyp.WAEHRUNG:
           if (getFeldWaehrung() != null)
