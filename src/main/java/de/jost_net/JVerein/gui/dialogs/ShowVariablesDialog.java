a8339de1c36eb91f3accc59f78c729d18e89f3bd
lenilsas <167138849+lenilsas@users.noreply.github.com>
2026-09-14 13:32:19 +0200

    Fix Cast

--- src/main/java/de/jost_net/JVerein/gui/dialogs/ShowVariablesDialog.java
+++ src/main/java/de/jost_net/JVerein/gui/dialogs/ShowVariablesDialog.java
@@ -91,7 +91,7 @@ public class ShowVariablesDialog extends AbstractJVereinDialog<Object>
     }
     tab = new JVereinTablePart(list, getCopyAction());
     tab.addColumn("Name", "name");
-    tab.addColumn("Wert", "wert", v -> ((String) v).split("\n")[0]);
+    tab.addColumn("Wert", "wert", v -> v.toString().split("\n")[0]);
     tab.paint(parent);
 
     ButtonArea buttons = new ButtonArea();
