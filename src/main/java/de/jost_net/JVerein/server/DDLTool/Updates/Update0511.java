281b43b8598f9b7deda6a1b5afa7a6d1cc736eb4
lenilsas <167138849+lenilsas@users.noreply.github.com>
2026-09-22 13:54:04 +0200

    Fix update

--- src/main/java/de/jost_net/JVerein/server/DDLTool/Updates/Update0511.java
+++ src/main/java/de/jost_net/JVerein/server/DDLTool/Updates/Update0511.java
@@ -37,11 +37,11 @@ public class Update0511 extends AbstractDDLUpdate
         + Property.QRCODETEXTVELOCITY.getKey() + "', CONCAT(" +
 
         // QRCODEFESTERTEXT
-        "CASE WHEN q.festerText = 1 THEN CONCAT(" +
+        "CASE WHEN q.festerText = '1' THEN CONCAT(" +
 
         // QRCODESNGLLINE:
         // Bei genau einer Zeile $ZAHLUNGSGRUND, sonst QRCODETEXT
-        "CASE WHEN q.singleLine = 1 THEN '#if($"
+        "CASE WHEN q.singleLine = '1' THEN '#if($"
         + RechnungVar.ZAHLUNGSGRUND.getName()
         + ".split(\"\\\\n\").size() == 1)$"
         + RechnungVar.ZAHLUNGSGRUND.getName() + "#{else}' ELSE '' END, " +
@@ -50,41 +50,41 @@ public class Update0511 extends AbstractDDLUpdate
         "q.text, " +
 
         // Ende des Velocity-if
-        "CASE WHEN q.singleLine = 1 THEN '#end' ELSE '' END, " +
+        "CASE WHEN q.singleLine = '1' THEN '#end' ELSE '' END, " +
 
         // Komma nach Festtext
-        "CASE WHEN q.datum = 1 OR q.rechnung = 1 "
-        + "OR q.mitglied = 1 THEN ', ' ELSE '' END" +
+        "CASE WHEN q.datum = '1' OR q.rechnung = '1' "
+        + "OR q.mitglied = '1' THEN ', ' ELSE '' END" +
 
         ") ELSE '' END, " +
 
         // Rechnung / Re.
-        "CASE WHEN q.datum = 1 OR q.rechnung = 1 THEN "
-        + "CASE WHEN q.kuerzen = 1 THEN 'Re. ' ELSE 'Rechnung ' END "
+        "CASE WHEN q.datum = '1' OR q.rechnung = '1' THEN "
+        + "CASE WHEN q.kuerzen = '1' THEN 'Re. ' ELSE 'Rechnung ' END "
         + "ELSE '' END, " +
 
         // Rechnungsnummer
-        "CASE WHEN q.rechnung = 1 THEN '$" + RechnungVar.NUMMER.getName()
+        "CASE WHEN q.rechnung = '1' THEN '$" + RechnungVar.NUMMER.getName()
         + "' ELSE '' END, " +
 
         // Rechnungsdatum
-        "CASE WHEN q.datum = 1 THEN CONCAT("
-        + "CASE WHEN q.rechnung = 1 THEN ' ' ELSE '' END, "
-        + "CASE WHEN q.kuerzen = 1 THEN 'v. ' ELSE 'vom ' END, '$"
+        "CASE WHEN q.datum = '1' THEN CONCAT("
+        + "CASE WHEN q.rechnung = '1' THEN ' ' ELSE '' END, "
+        + "CASE WHEN q.kuerzen = '1' THEN 'v. ' ELSE 'vom ' END, '$"
         + RechnungVar.DATUM.getName() + "') ELSE '' END, " +
 
         // Komma vor Mitglied
-        "CASE WHEN q.mitglied = 1 AND (q.datum = 1 OR q.rechnung = 1) "
+        "CASE WHEN q.mitglied = '1' AND (q.datum = '1' OR q.rechnung = '1') "
         + "THEN ', ' ELSE '' END, " +
 
         // Mitglied / Mitgl.
-        "CASE WHEN q.mitglied = 1 THEN "
-        + "CASE WHEN q.kuerzen = 1 THEN 'Mitgl. ' ELSE 'Mitglied ' END "
+        "CASE WHEN q.mitglied = '1' THEN "
+        + "CASE WHEN q.kuerzen = '1' THEN 'Mitgl. ' ELSE 'Mitglied ' END "
         + "ELSE '' END, " +
 
         // Mitgliedsnummer
-        "CASE WHEN q.mitglied = 1 THEN "
-        + "CASE WHEN q.externeMitgliedsnummer = 1 THEN '$"
+        "CASE WHEN q.mitglied = '1' THEN "
+        + "CASE WHEN q.externeMitgliedsnummer = '1' THEN '$"
         + MitgliedVar.EXTERNE_MITGLIEDSNUMMER.getName() + "' ELSE '$"
         + MitgliedVar.ID.getName() + "' END ELSE '' END" +
 
@@ -93,28 +93,28 @@ public class Update0511 extends AbstractDDLUpdate
         "FROM (SELECT " +
 
         // QRCODEFESTERTEXT
-        "MAX(CASE WHEN name = 'qrcodeptext' THEN wert ELSE '0' END) "
-        + "AS festerText, " +
+        "MAX(CASE WHEN name = 'qrcodeptext' THEN CASE WHEN UPPER(CAST(wert AS CHAR)) IN ('1', 'TRUE') "
+        + "THEN '1' ELSE '0' END  ELSE '0' END) AS festerText, " +
 
         // QRCODESNGLLINE
-        "MAX(CASE WHEN name = 'qrcodesngl' THEN wert ELSE '0' END) "
-        + "AS singleLine, " +
+        "MAX(CASE WHEN name = 'qrcodesngl' THEN CASE WHEN UPPER(CAST(wert AS CHAR)) IN ('1', 'TRUE') "
+        + "THEN '1' ELSE '0' END  ELSE '0' END) AS singleLine, " +
 
         // QRCODEDATUM
-        "MAX(CASE WHEN name = 'qrcodepdate' THEN wert ELSE '0' END) "
-        + "AS datum, " +
+        "MAX(CASE WHEN name = 'qrcodepdate' THEN CASE WHEN UPPER(CAST(wert AS CHAR)) IN ('1', 'TRUE') "
+        + "THEN '1' ELSE '0' END  ELSE '0' END) AS datum, " +
 
         // QRCODERENU
-        "MAX(CASE WHEN name = 'qrcodeprenum' THEN wert ELSE '0' END) "
-        + "AS rechnung, " +
+        "MAX(CASE WHEN name = 'qrcodeprenum' THEN CASE WHEN UPPER(CAST(wert AS CHAR)) IN ('1', 'TRUE') "
+        + "THEN '1' ELSE '0' END  ELSE '0' END) AS rechnung, " +
 
         // QRCODEMEMBER
-        "MAX(CASE WHEN name = 'qrcodepmnum' THEN wert ELSE '0' END) "
-        + "AS mitglied, " +
+        "MAX(CASE WHEN name = 'qrcodepmnum' THEN CASE WHEN UPPER(CAST(wert AS CHAR)) IN ('1', 'TRUE') "
+        + "THEN '1' ELSE '0' END  ELSE '0' END) AS mitglied, " +
 
         // QRCODEKUERZEN
-        "MAX(CASE WHEN name = 'qrcodekuerzen' THEN wert ELSE '0' END) "
-        + "AS kuerzen, " +
+        "MAX(CASE WHEN name = 'qrcodekuerzen' THEN CASE WHEN UPPER(CAST(wert AS CHAR)) IN ('1', 'TRUE') "
+        + "THEN '1' ELSE '0' END  ELSE '0' END) AS kuerzen, " +
 
         // QRCODETEXT
         "MAX(CASE WHEN name = 'qrcodetext' THEN wert ELSE '' END) "
@@ -122,7 +122,8 @@ public class Update0511 extends AbstractDDLUpdate
 
         // EXTERNEMITGLIEDSNUMMER bleibt als Property erhalten
         "MAX(CASE WHEN name = '" + Property.EXTERNEMITGLIEDSNUMMER.getKey()
-        + "' THEN wert ELSE 0 END) AS externeMitgliedsnummer " +
+        + "' THEN CASE WHEN UPPER(CAST(wert AS CHAR)) IN ('1', 'TRUE') "
+        + "THEN '1' ELSE '0' END  ELSE '0' END) AS externeMitgliedsnummer " +
 
         "FROM einstellungneu) q");
 
