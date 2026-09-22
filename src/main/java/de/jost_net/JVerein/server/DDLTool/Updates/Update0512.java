48d04d0a088b7cdef282074bdbe7fa33f4c16c80
Johann Maierhofer <johann.maierhofer@freenet.de>
2026-09-22 18:41:03 +0200

    Update Update0512.java

--- src/main/java/de/jost_net/JVerein/server/DDLTool/Updates/Update0512.java
+++ src/main/java/de/jost_net/JVerein/server/DDLTool/Updates/Update0512.java
@@ -19,9 +19,9 @@ import de.jost_net.JVerein.server.DDLTool.AbstractDDLUpdate;
 import de.willuhn.util.ApplicationException;
 import de.willuhn.util.ProgressMonitor;
 
-public class Update0511 extends AbstractDDLUpdate
+public class Update0512 extends AbstractDDLUpdate
 {
-  public Update0511(String driver, ProgressMonitor monitor, Connection conn)
+  public Update0512(String driver, ProgressMonitor monitor, Connection conn)
   {
     super(driver, monitor, conn);
   }
